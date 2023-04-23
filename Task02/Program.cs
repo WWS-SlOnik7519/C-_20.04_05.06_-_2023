8932e2a1199d90ce8b3d8c71a5895bb185e79506
Vyacheslav <5182116@mail.ru>
2023-04-23 15:08:44 +0300

    Added Task 02, 04, 06, 08

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,9 +1,9 @@
 ﻿// Напишите программу, которая на вход принимает два числа
 // и выдаёт, какое число большее, а какое меньшее.
 
-Console.Write("Введите число: ");
+Console.Write("Введите целое число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число: ");
+Console.Write("Введите второе целое число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 if (number1 > number2)
 {
