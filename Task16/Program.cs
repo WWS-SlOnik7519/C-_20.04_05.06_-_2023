c688c66760733ae943aca8752398325393a9088c
Vyacheslav <5182116@mail.ru>
2023-04-26 18:53:55 +0300

    Added Task16

---
+++ Task16/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая 
+// принимает на вход два числа
+// и проверяет, является ли одно число квадратом другого
+
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool result = IsSquare(number1, number2);
+Console.WriteLine(result ? "Да" : "Нет");
+
+bool IsSquare(int num1, int num2)
+{
+    return num2 == num1 * num1 || num1 == num2 * num2;
+}
