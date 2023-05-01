2db06b8b721515e00727d6df157bc3443adef7fc
Vyacheslav <5182116@mail.ru>
2023-05-01 18:14:12 +0300

    Added Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,18 @@
+﻿// 23: Напишите программу, которая 
+// принимает на вход число (N) и 
+// выдаёт таблицу кубов чисел от 1 до N
+
+Console.Write("Введите положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Cube (number);
+
+void Cube (int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine ($"{count,3}  {count * count * count,5}");
+        count ++;
+    }
+}
