6e29a99a512efff04e20beb310ad1f553ea7e4a5
Vyacheslav <5182116@mail.ru>
2023-05-19 20:38:18 +0300

    Added Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 26: Напишите программу, которая 
+// принимает на вход число и 
+// выдаёт количество цифр в числе.
+
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = CountNumbers(number);
+Console.WriteLine($"Кол-во цифр в числе {number} =  {count}");
+
+
+int CountNumbers (int num)
+{
+    int i = 0;
+    while (num != 0) 
+    {
+        num = num / 10;
+        i++;
+    }
+    return i;
+}
