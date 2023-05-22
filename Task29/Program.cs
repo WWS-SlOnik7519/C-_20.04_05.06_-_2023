e71fa8e7fb67497270c54003c23deb8ae6237de5
Vyacheslav <5182116@mail.ru>
2023-05-22 20:17:58 +0300

    Added Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 29: Напишите программу, 
+// которая задаёт массив из 8 элементов, 
+// заполненный псевдослучайными числами 
+// и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19] 
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int[] newArr = CreateArray(8);
+Console.Write("[");
+PrintArray(newArr);
+Console.Write("]");
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+}
+
+
+int[] CreateArray(int size)
+{
+    int[] array = new int [size];
+    var rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0,9);
+    }
+    return array;
+}
