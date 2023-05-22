838e64742a412fcbeb53babdd8db1bcf72629afe
Vyacheslav <5182116@mail.ru>
2023-05-22 18:22:34 +0300

    Save Task25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -5,46 +5,27 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-Console.Write("Введите число А: ");
+Console.Write("Введите натуральное число А: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число B: ");
+Console.Write("Введите натуральное число B: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
 
-if (numberB >= 0)
+if (numberB > 0)
 {
     int power = Exponent(numberA, numberB);
-Console.WriteLine($"Число {numberA} в степени {numberB} равно {power}");
+    Console.WriteLine($"Число {numberA} в степени {numberB} равно {power}");
 }
 else
 {
-    Console.WriteLine("Это у меня не получилось, дает ноль");
+    Console.WriteLine("Некорректный ввод!");
 }
 
 int Exponent(int a, int b)
 {
-    if (b > 0)
-    {
-        int pow = a;
+            int pow = a;
         for (int i = 2; i <= b; i++)
         {
             pow = pow * a;
         }
         return pow;
-    }
-    // else if (b < 0)
-    // {
-    //     int pow = ($"Это у меня не получилось, дает ноль");
-    //     int pow = 1 / a;
-    //      for (int i = -2; i >= b; i = i - 1)
-    //      {
-    //          pow = pow * (1 / a);
-    //      }
-    //     return pow;
-    // }
-
-    else
-    {
-        int pow = 1;
-        return pow;
-    }
-}
+   }
