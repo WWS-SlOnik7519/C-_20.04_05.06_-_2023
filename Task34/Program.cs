6bbdc701d9188f3d46e8c3b45321d233cd8d990a
Vyacheslav <5182116@mail.ru>
2023-05-24 20:01:22 +0300

    Added Task34

---
+++ Task34/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 34: 
+// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+
+int[] CreateArray(int size)
+{
+    int[] array = new int [size];
+    var rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(100,1000);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+}
+
+int QuantityEvenNum(int[] arr)
+{
+    int result = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) result += 1;
+    }
+    return result;
+}
+
+
+int[] newArray =  CreateArray(4);
+int quantityEvenNum = QuantityEvenNum(newArray);
+
+Console.Write("[");
+PrintArray(newArray);
+Console.Write("] -> ");
+Console.Write($"{quantityEvenNum}");
