c9eedf9dbcf87ffad146257f01a6dcac3fe41d17
Vyacheslav <5182116@mail.ru>
2023-05-30 19:29:43 +0300

    Added Task49

---
+++ Task49/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 49: Задайте двумерный массив.
+// Найдите элементы, у которых оба индекса чётные, 
+// и замените эти элементы на их квадраты.
+
+
+int[,] CreateMatrixFormula(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // rows = arr.GetLength (0)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // columns = arr.GetLength (1)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        //Console.Write ($"| {i}"); //номер строки i
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5} "); // 5 - форматирование отступа
+        }
+        //Console.Write ("|");
+        Console.WriteLine();
+    }
+}
+
+int[,] ArraySquareElementsWithEvenIndex(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i += 2)
+    {
+        for (int j = 0; j < matr.GetLength(1); j += 2)
+        {
+            matr[i, j] *= matr[i, j];
+        }
+    }
+    return matr;
+}
+
+
+Console.WriteLine("Введите число строк");
+int rows2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов");
+int columns2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите минимальное число в массиве");
+int minimum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное число в массиве");
+int maximum = Convert.ToInt32(Console.ReadLine());
+
+int[,] array2d = CreateMatrixFormula(rows2, columns2, minimum, maximum); //лучше пользовательский ввод
+PrintMatrix(array2d);
+ArraySquareElementsWithEvenIndex(array2d);
+Console.WriteLine("Массив с квадратами элементов с чётными индексами:");
+PrintMatrix(array2d);
